3a558c5fa692a35a7cfe1f192e23564f63b786a1
iancustefan26 <stefaniancu46@gmail.com>
2024-07-23 21:12:01 +0300

    sorted methods in different script files to keep all the things organized

--- src/scraper.py
+++ src/scraper.py
@@ -1,14 +1,5 @@
 from serpapi import GoogleSearch
-import json
-import re
-
-def check_isolated_word(string, word):
-    pattern = rf'(^|[^a-zA-Z]){word}([^a-zA-Z]|$)'
-    return re.search(pattern, string) is not None
-
-def check_isolated_letter(string, letter):
-    pattern = rf'(^|[^a-zA-Z]){letter}([^a-zA-Z]|$)'
-    return re.search(pattern, string) is not None
+from usable import *
 
 params = {
   "engine": "google_jobs",
