47225cf3ed901ad152043028352210c35df1e880
iancustefan26 <stefaniancu46@gmail.com>
2024-07-29 12:01:46 +0300

    removed unnecesary pieces of code

--- src/store_into_database.py
+++ src/store_into_database.py
@@ -4,6 +4,13 @@ from json_convertor import extract_countries
 import mysql.connector
 from usable import aboslute_path
 
+roles_id = {
+    "Software Engineer" : 1,
+    "Data Scientist" : 2,
+    "Machine Learning Engineer" : 3,
+    "Cloud Engineer" : 4,
+    "Cybersecurity Specialist" : 5
+}
 
 db_password = open(aboslute_path + "credentials/db_password.txt", "r").read(64).strip()
 
@@ -24,21 +31,3 @@ def store_for_one_role(table_name, role_id, skill_id, count, skill_column_name):
     cursor.execute(query, data)
     connection.commit()
     print(cursor.rowcount, "record inserted.")
-
-roles_id = {
-    "Software Engineer" : 1,
-    "Data Scientist" : 2,
-    "Machine Learning Engineer" : 3,
-    "Cloud Engineer" : 4,
-    "Cybersecurity Specialist" : 5
-}
-
-locations = extract_countries(90)
-
-store_for_one_role("role_languages", 1, 1, 3, "language_id")
-
-query = "SELECT * FROM role_languages"
-cursor.execute(query)
-results = cursor.fetchall()
-for row in results:
-    print(row)
