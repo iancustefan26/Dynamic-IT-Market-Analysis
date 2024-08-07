ea45c5e4ae80c358e233a291fb68c7b8a73fa041
iancustefan26 <stefaniancu46@gmail.com>
2024-08-07 13:02:57 +0300

    Implemented some good practices with Python

--- src/main.py
+++ src/main.py
@@ -4,19 +4,21 @@ from db_operations import store_for_one_role, close_database
 from scraper import roles
 from usable import table_names, skill_column_names, roles_id, skills_id
 
-locations = extract_countries(20)
 
-for role in roles:
-    results = get_skills_for_role(role, locations)
-    print(results)
-    for dict, table_name, column_name in zip(results, table_names, skill_column_names):
-        for skill in dict:
-            store_for_one_role(
-                table_name,
-                roles_id[role],
-                skills_id[skill],
-                dict[skill],
-                column_name
-            )
-            
-close_database()
+if __name__ == "__main__":
+    locations = extract_countries(20)
+
+    for role in roles:
+        results = get_skills_for_role(role, locations)
+        print(results)
+        for dict, table_name, column_name in zip(results, table_names, skill_column_names):
+            for skill in dict:
+                store_for_one_role(
+                    table_name,
+                    roles_id[role],
+                    skills_id[skill],
+                    dict[skill],
+                    column_name
+                )
+                
+    close_database()
